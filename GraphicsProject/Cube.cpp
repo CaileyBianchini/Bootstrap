e48deac08d9e34949208138119895b59d736def0
Cailey <cailey.m.bianchini@gmail.com>
2021-10-14 15:29:41 -0500

    almost have a cube made

--- GraphicsProject/Cube.cpp
+++ GraphicsProject/Cube.cpp
@@ -7,14 +7,61 @@ Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount)
 
 	vertices = new Vertex[vertexCount];
 
-	setVertexPosition(vertices, vertexCount, 0, { -1.0,  1.0f, -1.0f }); //A
-	setVertexPosition(vertices, vertexCount, 1, {  1.0,  1.0f,  1.0f }); //B
-	setVertexPosition(vertices, vertexCount, 2, {  1.0, -1.0f, -1.0f }); //C
-	setVertexPosition(vertices, vertexCount, 3, { -1.0, -1.0f,  1.0f }); //D
-	setVertexPosition(vertices, vertexCount, 4, {  1.0,  1.0f, -1.0f }); //E
-	setVertexPosition(vertices, vertexCount, 5, { -1.0,  1.0f,  1.0f }); //F
-	setVertexPosition(vertices, vertexCount, 6, { -1.0, -1.0f, -1.0f }); //G
-	setVertexPosition(vertices, vertexCount, 7, {  1.0, -1.0f,  1.0f }); //H
+	
+	//1
+	setVertexPosition(vertices, vertexCount, 0, { -1.0,  -1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 1, { -1.0f, -1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 2, { -1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 3, { 1.0f, 1.0f, -1.0f });
+
+	//2
+	setVertexPosition(vertices, vertexCount, 4, { -1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 5, { -1.0f, 1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 6, { 1.0f, -1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 7, { -1.0f, -1.0f, -1.0f });
+
+	//3
+	setVertexPosition(vertices, vertexCount, 8, { 1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 9, { 1.0f, 1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 10, { 1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 11, { -1.0f,-1.0f,-1.0f });
+
+	//4
+	setVertexPosition(vertices, vertexCount, 12, { -1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 13, { -1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 14, { -1.0f, 1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 15, { 1.0f,-1.0f, 1.0f });
+
+	//5
+	setVertexPosition(vertices, vertexCount, 16, { -1.0f,-1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 17, { -1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 18, { -1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 19, { -1.0f,-1.0f, 1.0f });
+
+	//6
+	setVertexPosition(vertices, vertexCount, 20, { 1.0f,-1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 21, { 1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 22, { 1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 23, { 1.0f, 1.0f,-1.0f });
+
+	//7
+	setVertexPosition(vertices, vertexCount, 20, { 1.0f,-1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 21, { 1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 22, { 1.0f,-1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 23, { 1.0f, 1.0f, 1.0f });
+
+	//8
+	setVertexPosition(vertices, vertexCount, 20, { 1.0f, 1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 21, { -1.0f, 1.0f,-1.0f });
+	setVertexPosition(vertices, vertexCount, 22, { 1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 23, { -1.0f, 1.0f,-1.0f });
+
+	//
+	setVertexPosition(vertices, vertexCount, 20, { -1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 21, { 1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 22, { -1.0f, 1.0f, 1.0f });
+	setVertexPosition(vertices, vertexCount, 23, { 1.0f,-1.0f, 1.0f });
+
 
 	return vertices;
 }
